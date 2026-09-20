f732808d89e4f23e0681e0a4782c22602b8965fb
MrDeliciousXX <206879863+MrDeliciousXX@users.noreply.github.com>
2026-09-20 13:04:04 +0200

    przebudowa struktury i dodanie nowych projektów

---
+++ data/projekty.js
@@ -0,0 +1,748 @@
+const projekty = {
+
+    22: {   tytul: 'Centrum Robotyki R&D w Gliwicach',
+
+        opis: 'Tu będzie opis projektu.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            },
+            {
+                plik: '3_plansza.jpg',
+                podpis: 'Plansza 3'
+            }
+        ]
+
+    },
+
+    21: {   tytul: 'Rydułtowy - koncepcja urbanistyczna',
+
+        opis: 'Tu będzie opis projektu.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            },
+            {
+                plik: '3_plansza.jpg',
+                podpis: 'Plansza 3'
+            }
+        ]
+    },
+
+    20: {   tytul: 'Pierścień - projektowanie parametryczne',
+
+        opis: 'Tu będzie opis projektu.',
+
+        obrazy: [
+            {
+                plik: '1_wizka.jpg',
+                podpis: 'Wizualizacja'
+            }
+        ]
+
+    },
+
+    19: {   tytul: '"Siedlisko" - projekt konkursowy',
+
+        opis: 'Tu będzie opis projektu.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza'
+            }
+        ]
+    },
+
+    18: {   tytul: 'Parowozowania - Rzeszów',
+
+        opis: 'Tu będzie opis projektu.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            },
+            {
+                plik: '3_plansza.jpg',
+                podpis: 'Plansza 3'
+            },
+            {
+                plik: '4_plansza.jpg',
+                podpis: 'Plansza 4'
+            }
+        ]
+
+    },
+
+    17: {   tytul: '"Serce Prudnika" - projekt łaźni',
+
+        opis: 'Tu będzie opis projektu łaźni.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            },
+            {
+                plik: '3_plansza.jpg',
+                podpis: 'Plansza 3'
+            },
+            {
+                plik: '4_wizka.jpg',
+                podpis: 'Wizualizacja 1'
+            },
+            {
+                plik: '5_wizka.jpg',
+                podpis: 'Wizualizacja 2'
+            },
+            {
+                plik: '6_wizka.jpg',
+                podpis: 'Wizualizacja 3'
+            },
+            {
+                plik: '7_rzut.jpg',
+                podpis: 'Rzut 1'
+            },
+            {
+                plik: '8_rzut.jpg',
+                podpis: 'Rzut 2'
+            },
+            {
+                plik: '9_przekroj.jpg',
+                podpis: 'Przekrój 1'
+            },
+            {
+                plik: '10_przekroj.jpg',
+                podpis: 'Przekrój 2'
+            }
+        ]
+    },
+
+    16: {   tytul: '"Familoki 2.0" - projekt inżynierski',
+
+        opis: 'Tu będzie opis projektu inżynierskiego.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            },
+            {
+                plik: '3_plansza.jpg',
+                podpis: 'Plansza 3'
+            },
+            {
+                plik: '4_plansza.jpg',
+                podpis: 'Plansza 4'
+            },
+            {
+                plik: '5_wizka.jpg',
+                podpis: 'Wizualizacja 1'
+            },
+            {
+                plik: '6_wizka.jpg',
+                podpis: 'Wizualizacja 2'
+            },
+            {
+                plik: '7_wizka.jpg',
+                podpis: 'Wizualizacja 3'
+            },
+            {
+                plik: '8_wizka.jpg',
+                podpis: 'Wizualizacja 4'
+            }
+        ]
+    },
+
+    15: {   tytul: 'Altanka dla zakochanych',
+
+        opis: 'Tu będzie opis projektu 10.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            },
+            {
+                plik: '3_wizka.jpg',
+                podpis: 'Wizualizacja'
+            }
+        ]
+
+    },
+
+    14: {   tytul: 'Centrum Zdrowia Psychicznego w Gliwicach',
+
+        opis: 'Tu będzie opis projektu 15.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            },
+            {
+                plik: '3_plansza.jpg',
+                podpis: 'Plansza 3'
+            },
+            {
+                plik: '4_wizka.jpg',
+                podpis: 'Wizualizacja 1'
+            },
+            {
+                plik: '5_wizka.jpg',
+                podpis: 'Wizualizacja 2'
+            },
+            {
+                plik: '6_wizka.jpg',
+                podpis: 'Wizualizacja 3'
+            }
+        ]
+
+    },
+
+    13: {    tytul: 'Projektowanie urbanistyczne zespołów mieszkaniowych w Dąbrowie Górniczej',
+
+        opis: 'Tu będzie opis projektu 8.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            },
+            {
+                plik: '3_plansza.jpg',
+                podpis: 'Plansza 3'
+            },
+            {
+                plik: '4_plansza.jpg',
+                podpis: 'Plansza 4'
+            },
+            {
+                plik: '5_plansza.jpg',
+                podpis: 'Plansza 5'
+            }
+        ],
+
+        slidery: [
+            {
+                tytul: 'Wizualizacje projektu',
+
+                obrazy: [
+                    {
+                        plik: '6_wizka.jpg',
+                        podpis: 'Wizualizacja 1'
+                    },
+                    {
+                        plik: '7_wizka.jpg',
+                        podpis: 'Wizualizacja 2'
+                    },
+                    {
+                        plik: '8_wizka.jpg',
+                        podpis: 'Wizualizacja 3'
+                    },
+                    {
+                        plik: '9_wizka.jpg',
+                        podpis: 'Wizualizacja 4'
+                    },
+                    {
+                        plik: '10_wizka.jpg',
+                        podpis: 'Wizualizacja 5'
+                    },
+                    {
+                        plik: '11_wizka.jpg',
+                        podpis: 'Wizualizacja 6'
+                    },
+                    {
+                        plik: '12_wizka.jpg',
+                        podpis: 'Wizualizacja 7'
+                    }
+                ]
+            },
+            {
+                tytul: 'Makieta',
+                
+                obrazy: [
+                    {
+                        plik: '13_makieta.jpg',
+                        podpis: 'Makieta 1'
+                    },
+                    {
+                        plik: '14_makieta.jpg',
+                        podpis: 'Makieta 2'
+                    },
+                    {
+                        plik: '15_makieta.jpg',
+                        podpis: 'Makieta 3'
+                    },
+                    {
+                        plik: '16_makieta.jpg',
+                        podpis: 'Makieta 4'
+                    },
+                    {
+                        plik: '17_makieta.jpg',
+                        podpis: 'Makieta 5'
+                    },
+                    {
+                        plik: '18_makieta.jpg',
+                        podpis: 'Makieta 6'
+                    },
+                    {
+                        plik: '19_makieta.jpg',
+                        podpis: 'Makieta 7'
+                    },
+                    {
+                        plik: '20_makieta.jpg',
+                        podpis: 'Makieta 8'
+                    }
+                ]
+            }
+        ]
+
+    },
+
+    12: {    tytul: 'Szafeczka sensoryczna',
+
+        opis: 'Tu będzie opis projektu 9.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            },
+            {
+                plik: '3_zdj.jpg',
+                podpis: 'Zdjęcie z wystawy INNI'
+            }
+        ]
+
+    },
+
+    11: {   tytul: 'Zabudowa mieszkaniowa wielorodzinna w Gliwicach',
+
+        opis: 'Tu będzie opis projektu 14.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            },
+            {
+                plik: '3_plansza.jpg',
+                podpis: 'Plansza 3'
+            },
+            {
+                plik: '4_plansza.jpg',
+                podpis: 'Plansza 4'
+            },
+            {
+                plik: '5_wizka.jpg',
+                podpis: 'Wizualizacja 1'
+            },
+            {
+                plik: '6_wizka.jpg',
+                podpis: 'Wizualizacja 2'
+            },
+            {
+                plik: '7_wizka.jpg',
+                podpis: 'Wizualizacja 3'
+            },
+            {
+                plik: '8_wizka.jpg',
+                podpis: 'Wizualizacja 4'
+            },
+            {
+                plik: '9_wizka.jpg',
+                podpis: 'Wizualizacja 5'
+            }
+        ]
+
+    },
+
+    10: {    tytul: 'Analizy Rybnika',
+
+        opis: 'Tu będzie opis projektu 7.',
+
+        obrazy: [
+            {
+                plik: '1_demografia.jpg',
+                podpis: 'Demografia'
+            },
+            {
+                plik: '2_warunki_naturalne.jpg',
+                podpis: 'Warunki naturalne, miasto na tle regionu'
+            },
+            {
+                plik: '3_miasto_na_tle_regionu.jpg',
+                podpis: 'Miasto na tle regionu'
+            },
+            {
+                plik: '4_kierunki_zagospodarowania_przestrzennego.jpg',
+                podpis: 'Kierunki zagospodarowania przestrzennego'
+            },
+            {
+                plik: '5_ miejscowe_plany_zagospodarowania.jpg',
+                podpis: 'Miejscowe plany zagospodarowania'
+            },
+            {
+                plik: '6_struktura_zagospodarowania_miasta.jpg',
+                podpis: 'Struktura zagospodarowania miasta'
+            },
+            {
+                plik: '7_stan_istniejacy.jpg',
+                podpis: 'Stan istniejący'
+            },
+            {
+                plik: '8_zielen.jpg',
+                podpis: 'Zieleń'
+            },
+            {
+                plik: '9_przestrzenie_publiczne.jpg',
+                podpis: 'Przestrzenie publiczne 1'
+            },
+            {
+                plik: '10_Przestrzenie_publiczne.jpg',
+                podpis: 'Przestrzenie publiczne 2'
+            },
+            {
+                plik: '11_analiza_swot_rybnik.jpg',
+                podpis: 'Analiza SWOT Rybnik'
+            }
+        ]
+
+    },
+
+    9: {   tytul: 'Centrum Promocji Nauki w Katowicach',
+        
+        opis: 'Tu będzie opis projektu 13.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza'
+            },
+            {
+                plik: '2_wizka.jpg',
+                podpis: 'Wizualizacja'
+            }
+        ]
+
+    },
+
+    8: {    tytul: 'Projekt zagospodarowania Alei Przyjaźni w Gliwicach',
+        
+        opis: 'Tu będzie opis projektu 6.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            },
+            {
+                plik: '3_plansza.jpg',
+                podpis: 'Plansza 3'
+            },
+            {
+                plik: '4_wizka.jpg',
+                podpis: 'Wizualizacja 1'
+            },
+            {
+                plik: '5_wizka.jpg',
+                podpis: 'Wizualizacja 2'
+            },
+            {
+                plik: '6_wizka.jpg',
+                podpis: 'Wizualizacja 3'
+            }
+        ]
+    },
+
+    7: {   tytul: 'Zabudowa mieszkaniowa jednorodzinna w Ożarowicach',
+
+        opis: 'Tu będzie opis projektu 12.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            }
+        ],
+
+        slidery: [
+            {
+                tytul: 'Makieta',
+
+                obrazy: [
+                    {
+                        plik: '3_zdj.jpg',
+                        podpis: 'Makieta 1'
+                    },
+                    {
+                        plik: '4_zdj.jpg',
+                        podpis: 'Makieta 2'
+                    },
+                    {
+                        plik: '5_zdj.jpg',
+                        podpis: 'Makieta 3'
+                    },
+                    {
+                        plik: '6_zdj.jpg',
+                        podpis: 'Makieta 4'
+                    },
+                    {
+                        plik: '7_zdj.jpg',
+                        podpis: 'Makieta 5'
+                    },
+                    {
+                        plik: '8_zdj.jpg',
+                        podpis: 'Makieta 6'
+                    },
+                    {
+                        plik: '9_zdj.jpg',
+                        podpis: 'Makieta 7'
+                    },
+                    {
+                        plik: '10_zdj.jpg',
+                        podpis: 'Makieta 8'
+                    },
+                    {
+                        plik: '11_zdj.jpg',
+                        podpis: 'Makieta 9'
+                    },
+                    {
+                        plik: '12_zdj.jpg',
+                        podpis: 'Makieta 10'
+                    },
+                    {
+                        plik: '13_zdj.jpg',
+                        podpis: 'Makieta 11'
+                    },
+                    {
+                        plik: '14_zdj.jpg',
+                        podpis: 'Makieta 12'
+                    }
+                ]
+            }
+        ]
+
+    },
+
+    6: {    tytul: 'Rekultywacja fragmentu osiedla "Sikornik" w Gliwicach',
+        
+        opis: 'Tu będzie opis projektu 5.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_plansza.jpg',
+                podpis: 'Plansza 2'
+            }
+        ]
+    },
+
+    5: {    tytul: 'Grafiki uzupełniające pisemną standaryzację tworzenia pizzy',
+
+        opis: 'Tu będzie opis projektu 4.',
+
+        obrazy: [
+            {
+                plik: '1_wizka.jpg',
+                podpis: 'Grafika 1'
+            },
+            {
+                plik: '2_wizka.jpg',
+                podpis: 'Grafika 2'
+            },
+            {
+                plik: '3_wizka.jpg',
+                podpis: 'Grafika 3'
+            }
+        ],
+
+        slidery: [
+            {
+                tytul: "Graficzne przedstawienie różnych rodzajów pizzy",
+
+                obrazy: [
+                    {
+                        plik: "4_pizza.jpg",
+                        podpis: "Pizza 1"
+                    },
+                    {
+                        plik: "5_pizza.jpg",
+                        podpis: "Pizza 2"
+                    },
+                    {
+                        plik: "6_pizza.jpg",
+                        podpis: "Pizza 3"
+                    },
+                    {
+                        plik: "7_pizza.jpg",
+                        podpis: "Pizza 4"
+                    },
+                    {
+                        plik: "8_pizza.jpg",
+                        podpis: "Pizza 5"
+                    },
+                    {
+                        plik: "9_pizza.jpg",
+                        podpis: "Pizza 6"
+                    }
+                ]
+            }
+        ]
+
+    },
+
+    4: {    tytul: 'Wizualizacje portierni przy Hucie Sosnowiec',
+
+        opis: 'Tu będzie opis projektu 3.',
+
+        obrazy: [
+            {
+                plik: '1_wizka.jpg',
+                podpis: 'Wizualizacja 1'
+            },
+            {
+                plik: '2_wizka.jpg',
+                podpis: 'Wizualizacja 2'
+            },
+            {
+                plik: '3_wizka.jpg',
+                podpis: 'Wizualizacja 3'
+            },
+            {
+                plik: '4_wizka.jpg',
+                podpis: 'Wizualizacja 4'
+            },
+            {
+                plik: '5_wizka.jpg',
+                podpis: 'Wizualizacja 5'
+            }
+        ]
+
+    },
+
+    3: {    tytul: 'Szkoła podstawowa w Afryce - Konkurs Kaira Looro 2023',
+
+        opis: 'Tu będzie opis projektu 2.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza'
+            },
+            {
+                plik: '2_wizka.jpg',
+                podpis: 'Wizualizacja'
+            }
+        ]
+
+    },
+
+    2: {   tytul: 'Szkółka surfingowa na Półwyspie Helskim',
+
+        opis: 'Tu będzie opis projektu 11.',
+
+        obrazy: [
+            {
+                plik: '1_plansza.jpg',
+                podpis: 'Plansza 1'
+            },
+            {
+                plik: '2_wizka.jpg',
+                podpis: 'Wizualizacja 1'
+            },
+            {
+                plik: '3_wizka.jpg',
+                podpis: 'Wizualizacja 2'
+            },
+            {
+                plik: '4_wizka.jpg',
+                podpis: 'Wizualizacja 3'
+            }
+        ]
+
+    },
+
+    1: {   tytul: 'Wizualizacje projektu domu jednorodzinnego',
+
+        opis: 'Tu będzie opis projektu 1.',
+
+        obrazy: [
+            {
+                plik: '1_wizka.jpg',
+                podpis: 'Wizualizacja 1'
+            },
+            {
+                plik: '2_wizka.jpg',
+                podpis: 'Wizualizacja 2'
+            },
+            {
+                plik: '3_wizka.jpg',
+                podpis: 'Wizualizacja 3'
+            },
+            {
+                plik: '4_wizka.jpg',
+                podpis: 'Wizualizacja 4'
+            }
+
+        ]
+    }
+
+};
